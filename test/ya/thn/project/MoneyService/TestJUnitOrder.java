945e800184918210bb35fd8afe89aeca5e10d499
Niklas Svensson <Niklas.Svensson@yaelev.se>
2020-04-20 18:41:40 +0200

    Added unitTests for classes: Currency, Order and Transaction

--- test/ya/thn/project/MoneyService/TestJUnitOrder.java
+++ test/ya/thn/project/MoneyService/TestJUnitOrder.java
@@ -1,58 +1,109 @@
 package ya.thn.project.MoneyService;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestJUnitOrder {
 	
-	List<Order> testList = new ArrayList<>();
-
-//	private void generateOrderList() {
-//	Order order1 = new Order(TransactionMode.BUY, 10, "USD");
-//	Order order2 = new Order(TransactionMode.BUY, 89, "JPY");
-//	Order order3 = new Order(TransactionMode.SELL, 37, "NOK");
-//	
-//	testList.add(order1);
-//	testList.add(order2);
-//	testList.add(order3);
-//	}
+	private static Order o1 = null;
 	
-	
-	@Test
-	public void testOrderMode1() {
-		Order testOrder = new Order(TransactionMode.BUY, 50, "USD");
-		TransactionMode mode = testOrder.getMode();
-
-		assertEquals(TransactionMode.BUY, mode);
+	@BeforeClass
+	public static void setUp() {
+		o1 = new Order(TransactionMode.BUY, 50, "USD");
 	}
 	
 	@Test
-	public void testOrderMode2() {
+	public void testAA1GetOrderNr_1() {
+		Order o2 = new Order(TransactionMode.SELL, 50, "USD");
+		assertEquals(2, o2.getOrderNr());
+	}
+	
+	@Test
+	public void testToString() {
+		assertEquals("Order [orderNr=1, mode=BUY, currencyCode=USD, amount=50]", o1.toString());
+	}
+		
+	@Test
+	public void testGetTransactionMode_1() {
+		assertEquals(TransactionMode.BUY, o1.getMode());
+	}
+	
+	@Test
+	public void testGetTransactionMode_2() {
 		Order testOrder = new Order(TransactionMode.SELL, 50, "USD");
-		TransactionMode mode = testOrder.getMode();
-
-		assertEquals(TransactionMode.SELL, mode);
+		assertEquals(TransactionMode.SELL, testOrder.getMode());
 	}
 	
 	@Test
-	public void testOrderNr1() {
-		//generateOrderList();
-		Order order1 = new Order(TransactionMode.BUY, 10, "USD");
-		Order order2 = new Order(TransactionMode.BUY, 89, "JPY");
-		Order order3 = new Order(TransactionMode.SELL, 37, "NOK");
-		
-		testList.add(order1);
-		testList.add(order2);
-		testList.add(order3);
-		
-		Order testOrder = testList.get(0);
-		System.out.println(testOrder.getOrderNr());
-		
-		assertEquals(1, testOrder.getOrderNr());
+	public void testGetCurrencyCode_1() {
+		assertEquals("USD", o1.getCurrencyCode());
 	}
+	
+	@Test
+	public void testGetAmount() {
+		assertEquals(50, o1.getAmount());
+	}
+	
+	@Test
+	public void testConstructorOK() {
+		Order o3 = new Order(TransactionMode.BUY, 100, "JPY");
+		assertNotNull(o3);
+	}
+	
+	@SuppressWarnings("unused")
+	@Test(expected = IllegalArgumentException.class)
+	public void testMissingCurrencyCode_1() {
+		Order o3 = new Order(TransactionMode.BUY, 100, "");
+	}
+	
+	@SuppressWarnings("unused")
+	@Test(expected = IllegalArgumentException.class)
+	public void testMissingCurrencyCode_2() {
+		Order o3 = new Order(TransactionMode.BUY, 100, null);
+	}
+	
+	@SuppressWarnings("unused")
+	@Test(expected = IllegalArgumentException.class)
+	public void testNegativeAmount() {
+		Order o3 = new Order(TransactionMode.SELL, -100, "USD");
+	}
+	
+	@SuppressWarnings("unused")
+	@Test(expected = IllegalArgumentException.class)
+	public void testZeroAmount() {
+		Order o3 = new Order(TransactionMode.SELL, 0, "USD");
+	}
+	
+	@Test
+	public void testEqual_1() {
+		Order o4 = new Order(TransactionMode.BUY, 200, "USD");
+		Order o5 = new Order(TransactionMode.BUY, 200, "USD");
+		
+		assertFalse(o4.equals(o5));
+	}
+	
+	
+	@Test
+	public void testHascode_1() {
+		Order o4 = new Order(TransactionMode.BUY, 200, "USD");
+		Order o5 = new Order(TransactionMode.BUY, 200, "USD");
+		
+		assertFalse(o4.hashCode() == o5.hashCode());
+	}
+	@Test
+	public void testHascode_2() {
+		Order o4 = new Order(TransactionMode.BUY, 200, "USD");
+		Order o5 = new Order(TransactionMode.BUY, 200, "USD");
+		
+		assertFalse(o4.hashCode() == o5.hashCode() && o4.equals(o5));
+	}
+	
+	
 }
--- test/ya/thn/project/MoneyService/TestJUnitTransaction.java
+++ test/ya/thn/project/MoneyService/TestJUnitTransaction.java
@@ -1,10 +1,123 @@
 package ya.thn.project.MoneyService;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestJUnitTransaction {
 
-
+	private static Transaction t1 = null;
+	
+	@BeforeClass
+	public static void setUp() {
+		t1 = new Transaction("USD", 500, TransactionMode.SELL);
+	}
+	
+	@Test
+	public void testConstructor_1() {
+		assertNotNull(t1);
+	}
+	
+	@Test
+	public void testAA1UniqueId_1() {
+		Transaction t2 = new Transaction("JPY", 1000, TransactionMode.BUY);
+		assertEquals(2, t2.getId());
+	}
+	
+	
+	@Test
+	public void testGetId() {
+		assertEquals(1, t1.getId());
+	}
+	
+	@Test
+	public void testGetAmount() {
+		assertEquals(500, t1.getAmount());
+	}
+	
+	@Test
+	public void testGetCurrencyCode() {
+		assertEquals("USD", t1.getCurrencyCode());
+	}
+	
+	@Test
+	public void testGetTransactionMode() {
+		assertEquals(TransactionMode.SELL, t1.getTransactionMode());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testMissingCurrencyCode_1() {
+		Transaction t3 = new Transaction("", 1000, TransactionMode.SELL);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testMissingCurrencyCode_2() {
+		Transaction t3 = new Transaction(null, 1000, TransactionMode.SELL);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testNegativeAmount() {
+		Transaction t4 = new Transaction("USD", -100, TransactionMode.BUY);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testZeroAmount() {
+		Transaction t4 = new Transaction("USD", 0, TransactionMode.SELL);
+	}
+	
+	@Test
+	public void testEquals_1() {
+		Transaction t5 = new Transaction("USD", 100, TransactionMode.SELL);
+		Transaction t6 = new Transaction("USD", 100, TransactionMode.SELL);
+		
+		assertFalse(t5.equals(t6));
+	}
+	
+	@Test
+	public void testEquals_2() {
+		Transaction t5 = new Transaction("USD", 100, TransactionMode.SELL);
+		Transaction t6 = new Transaction("JPY", 100, TransactionMode.SELL);
+		
+		assertFalse(t5.equals(t6));
+	}
+	
+	@Test
+	public void testEquals_3() {
+		Transaction t5 = new Transaction("USD", 100, TransactionMode.SELL);
+		Transaction t6 = new Transaction("USD", 50, TransactionMode.SELL);
+		
+		assertFalse(t5.equals(t6));
+	}
+	
+	@Test
+	public void testEquals_4() {
+		Transaction t5 = new Transaction("USD", 100, TransactionMode.SELL);
+		Transaction t6 = new Transaction("USD", 100, TransactionMode.BUY);
+		
+		assertFalse(t5.equals(t6));
+	}
+	
+	@Test
+	public void testHashCode_1() {
+		Transaction t5 = new Transaction("USD", 100, TransactionMode.SELL);
+		Transaction t6 = new Transaction("USD", 100, TransactionMode.SELL);
+		
+		assertFalse(t5.hashCode() == t6.hashCode());
+	}
+	
+	@Test
+	public void testHashCode_2() {
+		Transaction t5 = new Transaction("USD", 100, TransactionMode.SELL);
+		Transaction t6 = new Transaction("USD", 100, TransactionMode.SELL);
+		
+		assertFalse(t5.hashCode() == t6.hashCode() && t5.equals(t6));
+	}
+	
 }
